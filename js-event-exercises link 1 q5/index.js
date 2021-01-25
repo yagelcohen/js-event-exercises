4e5b875356f7db9efb1050fe0e4026d797285cd2
yagelcohen <77248177+yagelcohen@users.noreply.github.com>
2021-01-25 23:21:54 +0200

    tring stuff

--- js-event-exercises link 1 q5/index.js
+++ js-event-exercises link 1 q5/index.js
@@ -1,11 +1,14 @@
+list.hidden=true;
 
-ele.addEventListener('click', add1);
-
+function add1(){
+    list.hidden=false;
+}
 
 function rem(){
     list.hidden=true;
 }
-function add1(){
-    list.hidden=false;
-}
+
 ele.addEventListener('click' , rem);
+ele.addEventListener('click', add1);
+
+
---
+++ js-event-exercises link 2 q1/index.js
@@ -0,0 +1,15 @@
+const link = document.querySelector('a')
+
+link.addEventListener('click', exit )
+
+function exit(){
+    if(confirm("are you sure you want exit?") === true)
+    {link;
+    }else{
+
+    }
+
+}
+
+
+
