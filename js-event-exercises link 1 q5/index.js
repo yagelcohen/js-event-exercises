62bbbd4d78c1e302ff1fd65cd8631f8ac152ff88
yagelcohen <77248177+yagelcohen@users.noreply.github.com>
2021-01-25 19:50:41 +0200

    link1 q6 solved

---
+++ js-event-exercises link 1 q5/index.js
@@ -0,0 +1,11 @@
+
+ele.addEventListener('click', add1);
+
+
+function rem(){
+    list.hidden=true;
+}
+function add1(){
+    list.hidden=false;
+}
+ele.addEventListener('click' , rem);
---
+++ js-event-exercises link 1 q6/index/index.js
@@ -0,0 +1,23 @@
+
+
+
+
+button1.addEventListener('click', rem)
+
+
+function rem(){
+    section1.remove('div');
+}
+
+button2.addEventListener('click', rem2)
+
+
+function rem2(){
+    section2.remove('div');
+}
+button3.addEventListener('click', rem3)
+
+
+function rem3(){
+    section3.remove('div');
+}
