49766f7f627ae985f254cf1c4ebb5bd16d63fd73
yagelcohen <77248177+yagelcohen@users.noreply.github.com>
2021-01-26 15:11:50 +0200

    link 1 q5 solved

--- js-event-exercises link 1 q5/index.js
+++ js-event-exercises link 1 q5/index.js
@@ -1,14 +1,26 @@
+
+// function add1(){
+//     list.hidden=false;
+// }
+
+// function rem(){
+//     list.hidden=true;
+// }
+
+
+
+// ele.addEventListener('click' , rem);
+// ele.addEventListener('click', add1);
+
+
 list.hidden=true;
 
-function add1(){
-    list.hidden=false;
-}
-
-function rem(){
-    list.hidden=true;
-}
-
-ele.addEventListener('click' , rem);
-ele.addEventListener('click', add1);
-
-
+function myFunction() {
+    let x = document.getElementById("list");
+    if (x.hidden === true) {
+      x.hidden = false;
+    } else {
+      x.hidden = true;
+    }
+  }
+  ele.addEventListener('click' , myFunction);
