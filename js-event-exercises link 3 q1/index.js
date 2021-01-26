399834bd0c9fd83c4efa5ddf3199ba95da3103b9
yagelcohen <77248177+yagelcohen@users.noreply.github.com>
2021-01-26 20:15:23 +0200

    link 3 q1

--- js-event-exercises link 3 q1/index.js
+++ js-event-exercises link 3 q1/index.js
@@ -1,12 +1,16 @@
-let container = document.getElementsByClassName("container")
-container.onclick = function(event){
-  if(event.target.tagName !== "BUTTON"){
-              return;}
-       let rem = event.target.closest(".section");
-        rem.remove();
-    }
+// let container = document.getElementsByClassName("container")
+// container.onclick = function(event){
+//   if(event.target.tagName !== "BUTTON"){
+//               return;}
+//        let rem = event.target.closest(".section");
+//         rem.remove();
+//     }
 
-
+document.addEventListener('click',(event)=>{
+  if(event.target.className === 'button'){
+  event.target.parentElement.remove();
+  }
+  });
 
 
 
