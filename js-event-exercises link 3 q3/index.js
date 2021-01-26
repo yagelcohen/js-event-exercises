d013d1758b588c6252bbb757434f3045e2a122a1
yagelcohen <77248177+yagelcohen@users.noreply.github.com>
2021-01-26 21:27:29 +0200

    link 3 q 3

--- js-event-exercises link 3 q3/index.js
+++ js-event-exercises link 3 q3/index.js
@@ -1,1 +1,24 @@
+const tooltip = document.getElementById('tooltip');
+  
+document.addEventListener('mouseover', function(event) {
+  const elem = event.target;
+  const tooltipData = event.target.dataset.tooltip;
 
+  if(tooltipData) {
+    tooltip.innerHTML = tooltipData;
+
+    if((window.scrollY + tooltip.offsetHeight) > elem.offsetTop) {
+      tooltip.style.top = elem.offsetTop + elem.offsetHeight - window.scrollY+ 'px';
+    } else {
+      tooltip.style.top = elem.offsetTop - tooltip.offsetHeight - window.scrollY + 'px';
+    }
+
+    tooltip.style.left = elem.offsetLeft - window.scrollX + 'px';
+    tooltip.style.visibility = 'visible';
+  }
+
+});
+
+document.addEventListener('mouseout', function(event) {
+  const tooltipData = event.target.dataset.tooltip;
+});
